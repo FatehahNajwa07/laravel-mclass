854ec92a031f1cbebc861def7aa76380bdfa4535
Nur Fatehah Najwa Binti Mohd Fauzi <fatehah.fauzi@perodua.com.my>
2026-03-13 11:44:31 +0800

    update movie app

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Watchlist;
 
 class User extends Authenticatable
 {
@@ -46,4 +47,15 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function watchlist()
+    {
+        return $this->hasMany(Watchlist::class);
+    }
 }
+
